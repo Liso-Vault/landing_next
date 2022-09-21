b4e57ab77df6c8e7e79cf9c49b87502a49d9a037
nemoryoliver <nemoryoliver@gmail.com>
2022-09-21 09:31:33 +0800

    enlarged mockup size

--- components/Mockup.jsx
+++ components/Mockup.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Mockup = () => {
   return (
-    <div className="w-full max-w-[1000px] mx-auto">
+    <div className="w-full max-w-[1400px] mx-auto">
       <Image src={ImgMockup} alt="Liso Password Manager" />
     </div>
   );
